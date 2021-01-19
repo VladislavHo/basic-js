e8bc52ff5d47779db3ed7f3d0cbdea08166db0ba
vladislavho <introwlad@gmail.com>
2021-01-19 21:13:55 +0300

    status: cat task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
+module.exports = function countCats(cat) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
+    let coun = 0
+for (let i = 0; i < cat.length; i++) {
+  if (cat[i].includes('^^')) {
+    coun+=1
+  }else{
+    return coun
+  }
+  
+}
+return coun
 };
