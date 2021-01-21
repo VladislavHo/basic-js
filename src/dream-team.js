a81905db4e76623b01e7e5c2a377692394f74656
vladislavho <introwlad@gmail.com>
2021-01-21 15:54:00 +0300

    status: dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -4,15 +4,15 @@ module.exports = function createDreamTeam(members) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
   let arr = []
-  if(team == undefined || team === null || typeof(team) === 'boolean'){
+  if(members == undefined || members === null || typeof(members) === 'boolean'){
     return false
   }
-for (let i = 0; i < team.length; i++) {
-  if(typeof(team[i]) === 'string'){
-    arr.push(team[i][0].trim().toUpperCase())
+for (let i = 0; i < members.length; i++) {
+  if(typeof(members[i]) === 'string'){
+    arr.push(members[i][0].trim().toUpperCase())
   }
 
 }
-return team.length === 0 ? false : arr.sort().join('')
+return members.length === 0 ? false : arr.sort().join('')
 
 };
