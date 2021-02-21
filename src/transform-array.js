d09848adfcf2f34e2b63434f4744aa909b3960b9
vladislavho <introwlad@gmail.com>
2021-02-21 13:01:06 +0300

    feat: arr transform-array, not comlited!!

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports =  function transform(arr) {
+  if (!Array.isArray(arr)) {
+		throw new Error ()
+	}
+
+  let newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'string') {
+      if (arr[i] === '--discard-next') {
+        newArr.pop(arr[i + 1])
+        
+      }
+      if(arr[i] === '--discard-prev'){
+        newArr.pop(arr[i+1])
+        
+      }
+      if(arr[i] === '--double-next'){
+        newArr.push(arr[i+1])
+      }
+      if(arr[i] === '--double-prev'){
+        newArr.push(arr[i-1])
+      }
+      
+    }
+    if(typeof arr[i] === 'number'){
+      newArr.push(arr[i])
+    }
+    
+  }
+  return newArr
 };
+
+
