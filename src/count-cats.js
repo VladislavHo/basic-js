eac117c9e12b241cf84cd668c7a45e05ecb63bde
vladislavho <introwlad@gmail.com>
2021-03-02 11:46:32 +0300

    feat: conter cat

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,16 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(cat) {
-  // throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-let coun = 0
-for (let i = 0; i < cat.length; i++) {
-  if (cat[i].includes('^^')) {
-    coun+=1
-  }else{
-    return coun
-  }
-  
-}
-return coun
+module.exports = function countCats(matrix) {
+let count = 0
+matrix.flat().forEach(element => (element == "^^")? ++count : count)
+return count
 };
