8fd73ea973b56dd9a37cf5acc88fc7fe5f819700
vladislavho <introwlad@gmail.com>
2021-03-28 14:20:56 +0300

    feat: end Vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,71 @@
-const CustomError = require("../extensions/custom-error");
 
+const CustomError = require("../extensions/custom-error");
+let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(options = true){
+    this.options = options
   }
+
+  encrypt(messange, key) {
+    if(typeof messange == 'undefined' || typeof key == 'undefined'){
+      throw new Error()
+    }
+
+    
+    const upperMessange = messange.toUpperCase()
+    const upperKey = key.toUpperCase()
+
+    const arrLetters = [...letters]
+    const arrMessange = [...upperMessange]
+    const arrKey = [...upperKey]
+      // key
+    const keyAllString = arrMessange.map((elem, index) => elem = arrKey[index % arrKey.length])
+  //messange index
+    const indexMessangeLetter = arrMessange.map(elem =>arrLetters.indexOf(elem) == -1 ? elem : arrLetters.indexOf(elem))
+    const indexKeyLetter = keyAllString.map(elem => arrLetters.indexOf(elem))
+  
+    let keyIndex = 0 //index for cipherIndex
+  
+    const cipherIndex = indexMessangeLetter.map(elem => typeof elem !== 'number' ? elem || keyIndex++: elem + indexKeyLetter[keyIndex++])
+  
+    const correctChipherIndex = cipherIndex.map(elem => (elem >= letters.length ? elem - letters.length : elem))
+  
+    const chiher = correctChipherIndex.map(elem =>typeof elem !== 'number' ? elem : arrLetters[elem])
+
+    if(this.options == false){
+      return chiher.reverse().join('')
+    }
+    return chiher.join('')
+  }    
+  decrypt(messange, key) {
+    if(typeof messange == 'undefined' || typeof key == 'undefined'){
+      throw new Error()
+    }
+    const upperMessange = messange.toUpperCase()
+    const upperKey = key.toUpperCase()
+
+    const arrLetters = [...letters]
+    const arrMessange = [...upperMessange]
+    const arrKey = [...upperKey]
+// key all arr
+    const keyAllString = arrMessange.map((elem, index) => elem = arrKey[index % arrKey.length])
+  //messange index
+    const indexMessangeLetter = arrMessange.map(elem =>arrLetters.indexOf(elem) == -1 ? elem : arrLetters.indexOf(elem))
+    const indexKeyLetter = keyAllString.map(elem => arrLetters.indexOf(elem))
+  
+    let keyIndex = 0 //index for cipherIndex
+  
+    const cipherIndex = indexMessangeLetter.map(elem => typeof elem !== 'number' ? elem || keyIndex++: letters.length + elem - indexKeyLetter[keyIndex++])
+  
+    const correctChipherIndex = cipherIndex.map(elem => (elem >= letters.length ? elem - letters.length : elem))
+  
+    const chiher = correctChipherIndex.map(elem =>typeof elem !== 'number' ? elem : arrLetters[elem])
+    if(this.options == false){
+      return chiher.reverse().join('')
+    }
+    return chiher.join('')
+  }
+
 }
 
 module.exports = VigenereCipheringMachine;
